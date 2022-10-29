a45fb55271d571430b29319cde90b5105b699db6
derek-watson14 <derek.watson92@gmail.com>
2022-10-29 13:20:41 -0700

    finished web scrapers and webpage parsers

---
+++ data-retrieval/table_parser.py
@@ -0,0 +1,66 @@
+import os
+import csv
+from bs4 import BeautifulSoup
+from webscraper.webscraper.helpers import league_code_defs, color_code_defs
+
+# Open file to store data
+f = open("./data/table_data.csv", "w", encoding="utf-8", newline='')
+writer = csv.writer(f)
+
+# Write data headers
+column_headers = ["Country", "League", "Year", "Club", "Matchday", "Position", "Fate", "Matches", "Wins", "Draws", "Losses", "Goals For", "Goals Against", "Goal Differential", "Points"]
+writer.writerow(column_headers)
+
+# Set directory and loop through all files therein
+directory = "./webscraper/webpages"
+filename = os.listdir(directory)[0]
+
+bgcs = []
+bgcs_info = []
+
+for filename in os.listdir(directory):
+    # Print which file is being processed to show progress
+    print("Processing:", filename)
+
+    # Get information from filename
+    fn_data = filename[:-5].split("-")
+    year = fn_data[2]
+    league_code = fn_data[1]
+    country = league_code_defs[league_code]["country"]
+    league = league_code_defs[league_code]["league"]
+    matchday = int(fn_data[3][2:])
+
+    # Create path and try to access file
+    path = os.path.join(directory, filename)
+    if os.path.isfile(path):
+
+        # Get info from table
+        with open(path, "r", encoding="utf-8") as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+            table = soup.find("table", {"class": ["items"]}).find("tbody")
+            rows = table.find_all("tr")
+            for row in rows:
+                cells = row.find_all("td")
+                position = int(cells[0].text)
+                if cells[0].has_attr("style"):
+                    bgc = cells[0]["style"].split("#")[1]
+                else:
+                    bgc = None
+                fate = color_code_defs[bgc] if bgc in color_code_defs else "Mid Table"
+                club = cells[1].find("a")["title"]
+                matches = int(cells[3].text)
+                wins = int(cells[4].text)
+                draws = int(cells[5].text)
+                losses = int(cells[6].text)
+                goals = cells[7].text.split(":")
+                goals_for = int(goals[0])
+                goals_against = int(goals[1])
+                goal_diff = int(cells[8].text)
+                points = int(cells[9].text)
+
+            # Create list of data with above variables, write as row to CSV
+            data_row = [country, league, year, club, matchday, position, fate, wins, draws, losses, goals_for, goals_against, goal_diff, points]
+            writer.writerow(data_row)
+
+# Close CSV
+f.close()
---
+++ data-retrieval/webscraper/webscraper/spiders/table_spider.py
@@ -0,0 +1,50 @@
+import scrapy
+from pathlib import Path
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from helpers import seasons, league_code_defs
+
+# Serie A: 18 clubs 1988-2004, 20 clubs 2004 - present
+# Bundesliga: 18 clubs since 1992/93
+# Prem: 20 clubs since inception 92/93
+# La Liga: 22 clubs 95-97, 20 clubs 97-today
+# Ligue 1: 20 clubs 1970-1997, 18 clubs 1997-2002, 20 clubs 2002-2023, 18 clubs 2023-
+
+# Example page URL
+# https://www.transfermarkt.com/laliga/spieltagtabelle/wettbewerb/ES1?saison_id=1996&spieltag=42
+
+class TableSpider(scrapy.Spider):
+    name = "tables"
+
+    # Should download 2664 web pages
+    def start_requests(self):
+        urls = []
+        leagues = ["GB1", "L1", "IT1", "ES1", "FR1"]
+
+        for league in leagues:
+            for season in seasons:
+                matchdays = (league_code_defs[league]["size"] - 1) * 2
+                for matchday in range(1, matchdays+1):
+                    url = f"https://www.transfermarkt.com/laliga/spieltagtabelle/wettbewerb/{league}?saison_id={season}&spieltag={matchday}"
+                    urls.append(url) 
+
+        requests = 0
+        for url in urls:
+            requests += 1
+            self.log(f"Requests: {requests}")
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        u = response.url
+        league = u[u.find("b/")+2:u.find("?")]
+        season = u.split("&")[0][-4:]
+        matchday = u.split("=")[-1]
+        
+        base_path = Path(__file__).parent
+        file_path = (base_path / f"../webpages/table-{league}-{season}-md{matchday}.html").resolve()
+
+        with open(file_path, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {file_path}')
